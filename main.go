3720a8f44c6b1b49d1e09e3332ddb5eff520e56e
ophum <inagaki0106@gmail.com>
2022-07-29 00:26:57 +0900

    configファイルに対応

--- main.go
+++ main.go
@@ -12,18 +12,27 @@ import (
 
 	"github.com/ophum/k8s-secret-fs/pkg/fs"
 	"github.com/ophum/k8s-secret-fs/pkg/k8s"
+	"gopkg.in/yaml.v3"
 
 	"k8s.io/client-go/util/homedir"
 )
 
 var (
-	client     *k8s.Client
-	namespace  string
-	secretName string
-	mountPoint string
+	client *k8s.Client
+	config *Config
 )
 
+type Config struct {
+	Kubeconfig string `yaml:"kubeconfig"`
+	Namespace  string `yaml:"namespace"`
+	SecretName string `yaml:"secretName"`
+	MountPoint string `yaml:"mountPoint"`
+}
+
 func init() {
+	var configPath string
+	flag.StringVar(&configPath, "config", "", "-config config.yaml")
+
 	var kubeconfig *string
 	if home := homedir.HomeDir(); home != "" {
 		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
@@ -32,23 +41,36 @@ func init() {
 	}
 	flag.Parse()
 
-	namespacedSecretName := flag.Arg(0)
-	splited := strings.Split(namespacedSecretName, "/")
-	switch len(splited) {
-	case 1:
-		namespace = "default"
-		secretName = splited[0]
-	case 2:
-		namespace = splited[0]
-		secretName = splited[1]
-	default:
-		log.Panic("invalid namespace/secretName")
+	config = &Config{}
+	if configPath == "" {
+		config.Kubeconfig = *kubeconfig
+		namespacedSecretName := flag.Arg(0)
+		splited := strings.Split(namespacedSecretName, "/")
+		switch len(splited) {
+		case 1:
+			config.Namespace = "default"
+			config.SecretName = splited[0]
+		case 2:
+			config.Namespace = splited[0]
+			config.SecretName = splited[1]
+		default:
+			log.Panic("invalid namespace/secretName")
+		}
+		config.MountPoint = flag.Arg(1)
+	} else {
+		f, err := os.Open(configPath)
+		if err != nil {
+			log.Panic(err)
+		}
+		defer f.Close()
+
+		if err := yaml.NewDecoder(f).Decode(config); err != nil {
+			log.Panic(err)
+		}
 	}
 
-	mountPoint = flag.Arg(1)
-
 	var err error
-	client, err = k8s.NewClient(*kubeconfig, namespace, secretName)
+	client, err = k8s.NewClient(config.Kubeconfig, config.Namespace, config.SecretName)
 	if err != nil {
 		log.Panic(err)
 	}
@@ -56,7 +78,7 @@ func init() {
 
 func main() {
 	secretFS := fs.NewSecretFS(client)
-	server, err := secretFS.Mount(mountPoint)
+	server, err := secretFS.Mount(config.MountPoint)
 	if err != nil {
 		log.Panic(err)
 	}
